9b7b657c4dd4b42f566cce11e682849d79ee6d65
Eliel Delfino <eliel.delfino@fanut.ufal.br>
2026-04-15 13:59:19 -0300

    adicionando lifecycle para nao reverter deploys do work-flow

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -245,6 +245,11 @@ resource "aws_ecs_service" "app" {
     container_port   = var.container_port_nginx
   }
 
+  # Ignorar mudanças de task_definition para nao reverter deploys feitos pelo workflow do app 
+  lifecycle{
+    ignore_changes = [task_definition]
+  }
+
   depends_on = [
     aws_autoscaling_group.ecs,
     aws_lb_listener.http
